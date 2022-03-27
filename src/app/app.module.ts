8da9f210dd0dbf05f12faf8346a2877ed5ce97e3
Rohit Girotra <rohitgirotra555@gmail.com>
2022-03-27 11:08:41 +0530

    Template driven foms completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,14 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { AppComponent } from './app.component';
 import {MatListModule} from '@angular/material/list';
-import { MatGridListModule } from '@angular/material';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatGridListModule } from '@angular/material/grid-list';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { FormsModule } from '@angular/forms'; 
 import 'hammerjs';
 
 import { MenuComponent } from './menu/menu.component';
@@ -17,13 +22,15 @@ import { FooterComponent } from './footer/footer.component';
 import { HomeComponent } from './home/home.component';
 import { ContactComponent } from './contact/contact.component';
 import { AboutComponent } from './about/about.component';
-
+import { LoginComponent } from './login/login.component';
 
 import { DishService } from './services/dish.service';
-
-import { AppRoutingModule } from './app-routing/app-routing.module';
 import { PromotionService } from './services/promotion.service';
 import { LeaderService } from './services/leader.service';
+
+import { AppRoutingModule } from './app-routing/app-routing.module';
+
+
 
 
 @NgModule({
@@ -35,7 +42,8 @@ import { LeaderService } from './services/leader.service';
     FooterComponent,
     HomeComponent,
     ContactComponent,
-    AboutComponent
+    AboutComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
@@ -46,7 +54,15 @@ import { LeaderService } from './services/leader.service';
     MatGridListModule,
     MatCardModule,
     MatButtonModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatDialogModule,
+    MatFormFieldModule, 
+    MatInputModule,
+    MatCheckboxModule,
+    FormsModule
+  ],
+  entryComponents: [
+    LoginComponent
   ],
   providers: [DishService,PromotionService,LeaderService],
   bootstrap: [AppComponent]
